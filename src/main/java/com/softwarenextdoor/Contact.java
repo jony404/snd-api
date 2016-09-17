605bdaf245b71b9b2ca20188ef88d89208af2c73
Juan Castelan <jony404@gmail.com>
2016-09-16 21:37:52 -0500

    Project 1st exercise

---
+++ src/main/java/com/softwarenextdoor/Contact.java
@@ -0,0 +1,85 @@
+/*
+ * Software Next Door
+ */
+package com.softwarenextdoor;
+
+import java.io.Serializable;
+
+/**
+ * 15/sept/2016
+ * @author Juan Castelan
+ */
+public class Contact implements Serializable {
+        
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;    
+
+	public Contact() {
+	
+	}
+	
+    public Contact(String firstName, String lastName, String email, String phone) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.email = email;
+    }
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    /**
+     * @return the phone
+     */
+    public String getPhone() {
+        return phone;
+    }
+
+    /**
+     * @param phone the phone to set
+     */
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    
+}
